33a54547aee2db08c1aa421cf75eed34b764ea7e
fengjunkai-lab <1064889673@qq.com>
2025-07-02 10:38:49 +0800

    Add files via upload

---
+++ rs_rrscode.m
@@ -0,0 +1,4 @@
+%函数名称：rs_rrscode
+function new_msg=rs_rrscode(in_msg,all_code_msg,all_msg)
+new_msg=rs_interpret(in_msg,all_code_msg,all_msg);
+return;
---
+++ rscode_dis.m
@@ -0,0 +1,7 @@
+%函数名称：rscoud_dis.m
+function dis=rscode_dis(code1,code2)
+dis=0;
+for i=1:7
+    temp=gf_dis(code1(i),code2(i));
+    dis=dis+temp;
+end
